45159f4c9f41f0ec6b956cf66669df476da26618
duykhoa <duykhoa12t@gmail.com>
2014-03-17 20:38:41 +0700

    Fix cover photo url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,16 @@
 module ApplicationHelper
   def cover_picture book
-    book.edition_key.each do |edition_key|
-      image_url = "http://covers.openlibrary.org/b/olid/#{edition_key}-S.jpg"
-      return image_url if exist_image_with_edition_key(edition_key)
+    if book
+      book.edition_key.each do |edition_key|
+        image_url = "http://covers.openlibrary.org/b/olid/#{edition_key}-S.jpg"
+        return image_url if exist_image_with_edition_key(edition_key)
+      end
     end
     '/not_found.jpeg'
   end
 
   def exist_image_with_edition_key edition_key
     size = FastImage.size "http://covers.openlibrary.org/b/olid/#{edition_key}-S.jpg"
-    (size[0] > 1 && size[1] > 1)
+    size && size[0] > 1 && size[1] > 1
   end
 end
