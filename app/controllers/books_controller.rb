bfb6d7a9291b47d4cb82178866c36d43f56fb350
duykhoa <duykhoa12t@gmail.com>
2014-03-18 00:35:06 +0700

    Add the show page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,4 +8,8 @@ class BooksController < ApplicationController
 
     render 'index'
   end
+
+  def show
+    @book = Openlibrary::Client.new.book(params[:id])
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,25 @@ module ApplicationHelper
     size = FastImage.size "http://covers.openlibrary.org/b/olid/#{edition_key}-S.jpg"
     size && size[0] > 1 && size[1] > 1
   end
+
+  def cover cover
+    cover['medium'] if cover
+  end
+
+  def published_place places
+    address = []
+    if places
+      address = places.inject([]) { |address, place| address << place['name'] }
+    end
+
+    address.join(', ')
+  end
+
+  def subjects subjects
+    subjects.map { |subject| subject['name'] }.join(', ') if subjects
+  end
+
+  def publishers publishers
+    publishers.map { |publisher| publisher['name'] }.join(', ') if publishers
+  end
 end
