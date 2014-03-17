4fd2d62b7834bc4e27f611e4114d8204c7fbb8f7
duykhoa <duykhoa12t@gmail.com>
2014-03-17 21:18:34 +0700

    Attempt to fix the search function

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,14 +1,19 @@
 class Book < ActiveRecord::Base
   CATEGORY = {title: "Name", author: "Author", subject: "Subject"}.freeze
+  PER_PAGE = 50
 
   class << self
     def search params
       client = Openlibrary::Client.new
       begin
-      client.search search_params(params)
+        client.search search_params(params), page(params), PER_PAGE
       rescue Exception
         nil
       end
+    end
+
+    def page params
+      params.has_key?(:page) ? params[:page].to_i : 1
     end
 
     def search_params params
---
+++ config/initializers/openlibrary.rb
@@ -0,0 +1,30 @@
+module Openlibrary
+  module Search
+    # Access the Openlibrary free-text search.
+    # This API is experimental but useful (http://openlibrary.org/dev/docs/api/search)
+    # This implementation only supports the first 100 results for simplicity
+
+    # Possible search parameter keys include
+    # :q (cross-field query)
+    # :author
+    # :publisher
+    # :isbn
+    # :title
+    # :person
+    # :place
+    # :subject
+
+    def search(search_params, page=1, limit=50, offset=0)
+      processed_params = search_params.is_a?(String) ? {q: search_params} : search_params
+      processed_params.merge!({page: page, limit: limit})
+      puts processed_params
+      data = request("/search.json", params: processed_params)
+      results = []
+      data["docs"].each do |result|
+        results << Hashie::Mash.new(result)
+      end
+      results
+    end
+
+  end
+end
