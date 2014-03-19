29804570c42b5b9daef1b3c825f0e88d74f5e9e6
duykhoa <duykhoa12t@gmail.com>
2014-03-19 07:15:50 +0700

    Add rspec test for Book model

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,20 +1,49 @@
 require 'spec_helper'
 
 describe Book do
-  describe ".search_params" do
-    context "with cateory name" do
-      let!(:params) { {category: "title", query: "book"} }
+  describe ".params_find_by_author" do
+    it "returns a params hash" do
+      expect(Book.params_find_by_author("query").has_key?(:author)).to be_true
+    end
+  end
 
-      it "returns search params with category key" do
-        expect(Book.search_params(params)).to include(:title)
+  describe ".params_find_by_subject" do
+    it "returns a params hash" do
+      expect(Book.params_find_by_subject("query").has_key?(:subject)).to be_true
+    end
+  end
+
+  describe ".search_by_priority" do
+    let!(:client) { double('Client') }
+
+    context "query is a params" do
+      it "return an array" do
+        allow(client).to receive(:search).and_return([[],0])
+        client.should_receive(:search).exactly(3).times.and_return([[],0])
+        results = Book.search_by_priority({query: "query"}, client)
+
+        expect(results.is_a? Array).to be_true
+        expect(results).to eq([[],0])
       end
     end
 
-    context "without cateory name" do
-      let!(:params) { {query: "book"} }
+    context "query is a subject" do
+      it "return an array" do
+        allow(client).to receive(:search).and_return([[],0], [[:book], 1])
+        client.should_receive(:search).exactly(2).times
+        results = Book.search_by_priority({query: "query"}, client)
 
-      it "returns search params with category key" do
-        expect(Book.search_params(params)).to eq("book")
+        expect(results.is_a? Array).to be_true
+      end
+    end
+
+    context "query is a author" do
+      it "return an array" do
+        allow(client).to receive(:search).and_return([[:author], 1])
+        client.should_receive(:search).exactly(1).times
+        results = Book.search_by_priority({query: "query"}, client)
+
+        expect(results.is_a? Array).to be_true
       end
     end
   end
